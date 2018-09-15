57b640bab1ddbcda08b56bf8f81eb73c27194cea
biancanalien <nalienebianca@gmail.com>
2018-09-14 21:51:07 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Welcome to the amazing</h2>
+        <h2>Welcome to the</h2>
         <h1 className="tfg">TECH FOR GIRLS</h1>
         <h1 className="challenge">CHALLENGE</h1>
+	<h1><iframe src="https://giphy.com/embed/o0vwzuFwCGAFO" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-hacker-webs-o0vwzuFwCGAFO">via GIPHY</a></p></h1>
       </div>
     );
   }
