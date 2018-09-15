3a45c3ffcaacd11f012a21cac3e58b6719eb4ee4
biancanalien <nalienebianca@gmail.com>
2018-09-14 21:45:11 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Welcome to the Amazing</h2>
+        <h2>Welcome to the amazing</h2>
         <h1 className="tfg">TECH FOR GIRLS</h1>
         <h1 className="challenge">CHALLENGE</h1>
       </div>
